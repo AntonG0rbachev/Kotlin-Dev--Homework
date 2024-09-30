38c803e1a3942d01a3c5097ca2500492dbc413b2
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-09-30 16:00:26 +0300

    add build tool version in dep and change impl paths

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,16 +33,16 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildToolsVersion = "34.0.0"
 }
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
+    implementation(libs.androidx.core.ktx.v190)
+    implementation(libs.androidx.appcompat.v170)
+    implementation(libs.material.v1120)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit.v121)
+    androidTestImplementation(libs.androidx.espresso.core.v361)
 }
